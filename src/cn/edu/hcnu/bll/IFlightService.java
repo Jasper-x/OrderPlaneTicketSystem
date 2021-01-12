2d3514bfb276706b0a5d9f891e7d43c9c038a035
Administrator <1132174760@qq.com>
2021-01-12 20:58:40 +0800

    初次上传

--- src/cn/edu/hcnu/bll/IFlightService.java
+++ src/cn/edu/hcnu/bll/IFlightService.java
@@ -2,6 +2,13 @@ package cn.edu.hcnu.bll;
 
 import cn.edu.hcnu.bean.Flight;
 
+import java.util.Set;
+
 public interface IFlightService {
-    void insertFlight(Flight flight);
+    void insterFlight (Flight flight);
+    Set<Flight> getAllFlights();
+    Flight getFlightByDepartureTime(String departureTime);
+    Flight getFlightByDeparturAirPort(String departurAirPort);
+    Flight getFlightByDestinationAirPort(String destinationAirPort);
+    void updateFlight(Flight flight);
 }
