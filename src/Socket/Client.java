068defa79666c6e4c43e0269b3a9d6147f72b8a3
Administrator <1132174760@qq.com>
2021-01-15 10:26:20 +0800

    初次上传

--- src/Socket/Client.java
+++ src/Socket/Client.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 public class Client {
     public static void main(String[] args) {
         try {
-            Socket socket=new Socket("192.168.1.35",8866);
+            Socket socket=new Socket("localhost",8888);
             InputStream is=socket
                     .getInputStream();
             InputStreamReader isr=new InputStreamReader(is);
--- src/Socket/SocketDemo.java
+++ src/Socket/SocketDemo.java
@@ -7,26 +7,22 @@ import java.net.Socket;
 public class SocketDemo {
     public static void main(String[] args) {
         try {
-            ServerSocket ss=new ServerSocket(8866);
-            Socket socket=ss.accept();
-            System.out.println("新用户进入系统");
+            Socket socket = null;
+            ServerSocket ss = new ServerSocket(8888);
+            while (true) {
+                System.out.println("等待新用户连接......");
+                socket = ss.accept();
+                System.out.println("新用户进入系统");
+                OutputStream os = socket.getOutputStream();//先找出该行有几个对象，然后在该行上面依次打印对象的值
+                OutputStreamWriter osw = new OutputStreamWriter(os);
+                PrintWriter out = new PrintWriter(osw);
+                out.println("欢迎你XXX");
+                out.flush();
 
-
-            OutputStream os=socket.getOutputStream();
-            OutputStreamWriter osw=new OutputStreamWriter(os);
-            PrintWriter out=new PrintWriter(osw);
-            out.println("欢迎你XXX");
-            out.flush();
-
-            while(true) {
-                InputStream is = socket
-                        .getInputStream();
-                InputStreamReader isr = new InputStreamReader(is);
-                BufferedReader in = new BufferedReader(isr);
-                String serverMessage = in.readLine();
-                System.out.println("客户端说：" + serverMessage);
+                //启动收消息线程
+                GetMessageThread getMessageThread = new GetMessageThread(socket);
+                getMessageThread.start();
             }
-
         } catch (IOException e) {
             e.printStackTrace();
         }
